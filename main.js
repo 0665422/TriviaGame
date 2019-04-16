8ac781f66a46d4c81d419769e25366217118c963
7071 BusLaptop <7071-Buslaptop@dadeschools.net>
2019-04-16 10:28:11 -0400

    started HTML and JS

---
+++ main.js
@@ -0,0 +1,29 @@
+var panel = $("#quiz-area");
+
+var countStartNumber = 30;
+
+
+// Question set
+var questions = [
+    {
+        question: "What is the name of the first operation in R6S?",
+        answer: ["Black Ice", "Chimera", "Blood Orchid", "White Noise"],
+        correctAnswer: "Black Ice",
+        image: "assets/black-ice.png"
+    },
+
+    {
+        question: "Who is the Spetsnaz operator?",
+        answer: ["Buck", "Fuze", "Finka", "Bandit"],
+        correctAnswer: "Fuze",
+        image: "assets/fuze.gif"
+    },
+
+    {
+        question: "Which operator uses an Active Defense system to intercept grenades?",
+        answer: ["Jager", "Dokkaebi", "Glaz", "Kapkan"],
+        correctAnswer: "Jager",
+        image: "assets/jager.gif"
+    },
+    
+];
