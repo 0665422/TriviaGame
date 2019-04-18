ed9c6124a1e99db639b10c60603dc12463fea71b
7071 BusLaptop <7071-Buslaptop@dadeschools.net>
2019-04-18 10:04:14 -0400

    questionLoad JS function

--- main.js
+++ main.js
@@ -21,9 +21,50 @@ var questions = [
 
     {
         question: "Which operator uses an Active Defense system to intercept grenades?",
-        answer: ["Jager", "Dokkaebi", "Glaz", "Kapkan"],
+        answer: ["Jager", "Kaid", "Kapkan", "Maestro"],
         correctAnswer: "Jager",
         image: "assets/jager.gif"
     },
     
 ];
+
+// Variable to hold our setInterval
+var timer;
+
+var game = {
+    questions: questions,
+    currentQuestion: 0,
+    counter: countStartNumber,
+    correct: 0,
+    incorrect: 0,
+
+    countdown: function() {
+        game.correct --;
+        $("#counter-number").html(game.counter);
+        if (game.counter === 0) {
+            console.log('Time Up!');
+            game.timeUp()
+        }
+    },
+
+    loadQuestion: function() {
+        timer = setInterval(game.counter, 1000);
+        panel.html("<h2>" + questions[this.currentQuestion] + "</h2>");
+        for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
+            panel.append("<button class+'answer-button' id='button' data-name=''" + 
+            questions[this.currentQuestion].answer[i] + ">" + 
+            questions[this.currentQuestion].answer[i] + "<button");
+        }
+    },
+
+    nextQuestion: function() {
+        game.counter = countStartNumber;
+        $("#counter-number").html(game.counter);
+        game.currentQuestion++;
+        game.loadQuestion();
+    },
+
+    timeUp: function() {
+        
+    }
+}
